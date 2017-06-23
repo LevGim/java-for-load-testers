bd9966a847df7348f60ad52de15ce681b43577fa
ЛЕВ <anton.sverskoi@yandex.ru>
2017-06-23 18:12:26 +0300

    new

---
+++ src/main/java/com/jet/edu/Analitic.java
@@ -0,0 +1,7 @@
+package com.jet.edu;
+
+/**
+ * Created by ЛЕВ on 23.06.2017.
+ */
+public class Analitic {
+}
---
+++ src/main/java/com/jet/edu/Chat.java
@@ -0,0 +1,18 @@
+package com.jet.edu;
+
+/**
+ * Created by ЛЕВ on 23.06.2017.
+ */
+public class Chat {
+
+public static void sendMessenger(){
+saveToHistory();
+}
+
+public static void receiveMessenger(){
+saveToHistory();
+}
+public static void saveToHistory(){
+
+    }
+}
