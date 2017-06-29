df8885bc720afc93a5388739738f9de2719d6397
ЛЕВ <anton.sverskoi@yandex.ru>
2017-06-30 01:25:49 +0300

    Calculate + double, proverka vvoda, vivoda, abs

--- src/main/java/com/jet/edu/Calculate.java
+++ src/main/java/com/jet/edu/Calculate.java
@@ -5,19 +5,45 @@ package com.jet.edu;
  */
 class Calculate {
 
+
+    public static boolean proverkaVvoda(int a, int b, boolean f){
+        f=true;
+        if (a<-10||a>10||b<-10||b>10){
+            f=false;
+        }
+        return f;
+    }
+
+    public static boolean proverkaVvoda(double a, double b, boolean f){
+        f=true;
+        if (a<-10.00||a>10.00||b<-10.00||b>10.00){
+            f=false;
+        }
+        return f;
+    }
+
     public static int sum(int a, int b) { // ищем и возвращаем сумму переменных
+        System.out.println("\nВыполнение операции сумма");
         return a + b;
     }
 
     public static int sub(int a, int b) { // ищем и возвращаем разность переменных
+        System.out.println("\nВыполнение операции разность");
         return a - b;
     }
 
     public static int mul(int a, int b) { // ищем и возвращаем произведение переменных
+        System.out.println("\nВыполнение операции произведение");
         return a * b;
     }
 
     public static int div(int a, int b) { // ищем и возвращаем частное переменных
+        System.out.println("\nВыполнение операции частное");
+        if(b==0){
+            System.out.println("На ноль делить нельзя! Операция не выполнена!\n" +
+                    "Проверьте введенные значения!\n");
+            a=0;b=1;
+        }
         return a / b;
     }
 
@@ -25,18 +51,27 @@ class Calculate {
     // double- переменные
 
     public static double sum(double a, double b) { // ищем и возвращаем сумму переменных
+        System.out.println("\nВыполнение операции сумма");
         return a + b;
     }
 
     public static double sub(double a, double b) { // ищем и возвращаем разность переменных
+        System.out.println("\nВыполнение операции разность");
         return a - b;
     }
 
     public static double mul(double a, double b) { // ищем и возвращаем произведение переменных
+        System.out.println("\nВыполнение операции произведение");
         return a * b;
     }
 
     public static double div(double a, double b) { // ищем и возвращаем частное переменных
+        System.out.println("\nВыполнение операции частное");
+        if(b==0){
+            System.out.println("На ноль делить нельзя! Операция не выполнена!\n" +
+                    "Проверьте введенные значения!\n");
+            a=0;b=1.0;
+        }
         return a / b;
     }
 
@@ -52,5 +87,54 @@ class Calculate {
         mem=mem+number1 +number2+number3+number4; // Суммируем все 4 результата операций
         // разность, сумма, произведение, частное и возвращаем.
         return mem;
+    }
+
+    public static int memory(int number1, int mem) {
+
+        mem=mem+number1; // Суммируем все 4 результата операций
+        // разность, сумма, произведение, частное и возвращаем.
+        return mem;
+    }
+    public static double memory(double number1, double mem) {
+
+        mem=mem+number1; // Суммируем все 4 результата операций
+        // разность, сумма, произведение, частное и возвращаем.
+        return mem;
+    }
+
+    public static int proverkaResult(int result, int i){
+
+        if(result<-10||result>10){
+            System.out.println("Результат операции превышает установленные пределы [-10:10]\n" +
+                    "Установите другие значения вычисляемых переменных!\n"+ "Результат: " + result + "\n");
+            result=0;
+        }
+        else {
+            if (i==1) System.out.println("Сумма чисел а и b:         " + result);
+            if (i==2) System.out.println("Разность чисел a и b:      " + result);
+            if (i==3) System.out.println("Произведение чисел a и b:  " + result);
+            if (i==4) System.out.println("Частное чисел a и b:       " + result);
+            if (i==5) System.out.println("Значение числа по модулю:  " + result);
+        }
+        return result;
+    }
+// Проверка для doubble
+
+
+    public static double proverkaResult(double result, int i){
+
+        if(result<-10||result>10){
+            System.out.println("Результат операции превышает установленные пределы [-10:10]\n" +
+                    "Установите другие значения вычисляемых переменных!\n"+ "Результат: " + result + "\n");
+            result=0;
+        }
+        else {
+            if (i==1) System.out.println("Сумма чисел c и d:         " + result);
+            if (i==2) System.out.println("Разность чисел c и d:      " + result);
+            if (i==3) System.out.println("Произведение чисел c и d:  " + result);
+            if (i==4) System.out.println("Частное чисел c и d:       " + result);
+            if (i==5) System.out.println("Значение числа по модулю:  " + result);
+        }
+        return result;
     }
    }
--- src/main/java/com/jet/edu/CalculateApp.java
+++ src/main/java/com/jet/edu/CalculateApp.java
@@ -4,58 +4,123 @@ package com.jet.edu;
  * Created by ЛЕВ on 26.06.2017.
  */
 public class CalculateApp {
+
+    private static int a=2, b=-10;
+    private static double c=.5, d=.4;
+
+
     public static void main(String[] args) {
-        //int operation=1;
-        int a=5,b=4,per=0,vto=0,tre=0,chet=0, mem=0;
+        int first=0,second=0,third=0,fourth=0, fifth=0, mem=0;
+        boolean pro=true;
 
-        for (int i=1; i<5; i++){
-            if(i==1){
-                per= Calculate.sum(a,b);
-                System.out.println("Сумма чисел а и b:         " + per);
+        System.out.println("Числа a и b соотвественно:  " + a + "  " + b);
+
+        pro=Calculate.proverkaVvoda(a, b, pro);
+        if (pro==true) {
+
+            for (int i = 1; i < 6; i++) {
+                if (i == 1) {
+                    first = Calculate.sum(a, b);
+                    first = Calculate.proverkaResult(first,i);
+                }
+                if (i == 2) {
+                    second = Calculate.sub(a, b);
+                    second = Calculate.proverkaResult(second,i);
+                }
+                if (i == 3) {
+                    third = Calculate.mul(a, b);
+                    third = Calculate.proverkaResult(third,i);
+                }
+                if (i == 4) {
+                    fourth = Calculate.div(a, b);
+                    fourth = Calculate.proverkaResult(fourth,i);
+                }
+                if (i == 5) {  // исходные числа по модулю
+                    if(a<0){
+                        fifth = Math.abs(a);
+                        fifth = Calculate.proverkaResult(fifth,i);
+                        mem=Calculate.memory(fifth,mem);
+                    }
+                     else{
+                        if(b<0) {
+                            fifth = Math.abs(b);
+                            fifth = Calculate.proverkaResult(fifth,i);
+                            mem = Calculate.memory(fifth, mem);
+                        }
+                        else System.out.println("\nВычислять модуль не требуется!");
+                     }
+                }
+
+
+                mem = Calculate.memory(first, second, third, fourth, mem);
+                System.out.println("Общая сумма операций:      " + mem);
+                first = 0;
+                second = 0;
+                third = 0;
+                fourth = 0;
             }
-            if (i==2){
-                vto= Calculate.sub(a,b);
-                System.out.println("Разность чисел а и b:      " + vto);
-            }
-            if (i==3){
-                tre= Calculate.mul(a,b);
-                System.out.println("Произведение чисел а и b:  " + tre);
-            }
-            if (i==4){
-                chet= Calculate.div(a,b);
-                System.out.println("Частное чисел а и b:       " + chet);
-            }
-            mem=Calculate.memory(per, vto, tre, chet, mem);
-            System.out.println("Общая сумма операций:      " + mem);
-            per=0; vto=0;tre=0;chet=0;
+
+            System.out.println("\n\n");
         }
-
-        System.out.println("\n\n");
+        else {
+            System.out.println("Вводимые числа не удовлетворяют условиям.\n" +
+                    "Они должны принадлежать промежутку от -10 до 10\n\n");
+        }
 
         //double - переменные.
 
-        double c=.5, d=.4, per1=.0, vto1=.0, tre1=.0, chet1=.0, mem1=.0;
 
-        for (int i=1; i<5; i++){
-            if(i==1){
-                per1= Calculate.sum(c,d);
-                System.out.println("Сумма чисел c и d:         " + per1);
+        double first1 = .0, second1 = .0, third1 = .0, fourth1 = .0, fifth1=.0, mem1 = .0;
+
+        System.out.println("Числа c и d соотвественно:  " + c + "  " + d);
+
+        pro=Calculate.proverkaVvoda(c, d, pro);
+        if (pro==true) {
+
+            for (int i = 1; i < 6; i++) {
+                if (i == 1) {
+                    first1 = Calculate.sum(c, d);
+                    first1 = Calculate.proverkaResult(first1,i);
+                }
+                if (i == 2) {
+                    second1 = Calculate.sub(c, d);
+                    second1 = Calculate.proverkaResult(second1,i);
+                }
+                if (i == 3) {
+                    third1 = Calculate.mul(c, d);
+                    third1 = Calculate.proverkaResult(third1,i);
+                }
+                if (i == 4) {
+                    fourth1 = Calculate.div(c, d);
+                    fourth1 = Calculate.proverkaResult(fourth1,i);
+                }
+                if (i == 5) {
+                    if(c<0){
+                        fifth1 = Math.abs(c);
+                        fifth1 = Calculate.proverkaResult(fifth1,i);
+                        mem1=Calculate.memory(fifth1,mem1);
+                    }
+                    else{
+                        if(d<0) {
+                            fifth1 = Math.abs(d);
+                            fifth1 = Calculate.proverkaResult(fifth1,i);
+                            mem1 = Calculate.memory(fifth1, mem1);
+                        }
+                        else System.out.println("\nВычислять модуль не требуется!");
+                    }
+
+                }
+                mem1 = Calculate.memory(first1, second1, third1, fourth1, mem1);
+                System.out.println("Общая сумма операций:      " + mem1);
+                first1 = .0;
+                second1 = .0;
+                third1 = .0;
+                fourth1 = .0;
             }
-            if (i==2){
-                vto1= Calculate.sub(c,d);
-                System.out.println("Разность чисел c и d:      " + vto1);
-            }
-            if (i==3){
-                tre1= Calculate.mul(c,d);
-                System.out.println("Произведение чисел c и d:  " + tre1);
-            }
-            if (i==4){
-                chet1= Calculate.div(c,d);
-                System.out.println("Частное чисел c и d:       " + chet1);
-            }
-            mem1=Calculate.memory(per1, vto1, tre1, chet1, mem1);
-            System.out.println("Общая сумма операций:      " + mem1);
-            per1=.0; vto1=.0; tre1=.0; chet1=.0;
+        }
+        else {
+            System.out.println("Вводимые числа не удовлетворяют условиям.\n" +
+                    "Они должны принадлежать промежутку от -10.00 до 10.00\n\n");
         }
     }
 }
